673f74b999a4264c23572cfc4fcde799691d15e2
Pontuagi <benepontuagi@gmail.com>
2024-02-12 10:29:44 +0300

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,41 @@
+// DBClient module
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor () {
+    const dbHost = process.env.DB_HOST || 'localhost';
+    const dbPort = process.env.DB_PORT || 27017;
+    const dbName = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect(err => {
+      if (err) {
+        console.error('MongoDB connection error:', err);
+      }
+    });
+  }
+
+  isAlive () {
+    return !!this.client && !!this.client.topology && this.client.topology.isConnected();
+  }
+
+  async nbUsers () {
+    const db = this.client.db();
+    const usersCollection = db.collection('users');
+    const count = await usersCollection.countDocuments();
+    return count;
+  }
+
+  async nbFiles () {
+    const db = this.client.db();
+    const filesCollection = db.collection('files');
+    const count = await filesCollection.countDocuments();
+    return count;
+  }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
