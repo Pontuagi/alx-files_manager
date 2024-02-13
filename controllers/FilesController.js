7a70124a9b25fa3df45d1374e2bac12bc54fae48
Pontuagi <benepontuagi@gmail.com>
2024-02-13 17:13:45 +0300

    First File

---
+++ controllers/FilesController.js
@@ -0,0 +1,67 @@
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+const FilesController = {
+  async postUpload(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const userId = await redisClient.get(`auth_${token}`);
+
+    const {
+      name, type, parentId = '0', isPublic = false, data,
+    } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Missing name' });
+    }
+    if (!type || !['folder', 'file', 'image'].includes(type)) {
+      return res.status(400).json({ error: 'Missing type' });
+    }
+    if (!data && type !== 'folder') {
+      return res.status(400).json({ error: 'Missing data' });
+    }
+
+    if (parentId !== '0') {
+      const parentFile = await dbClient.getFileById(parentId);
+      if (!parentFile) {
+        return res.status(404).json({ error: 'Parent not found' });
+      }
+      if (parentFile.type !== 'folder') {
+        return res.status(400).json({ error: 'Parent is not a folder' });
+      }
+    }
+    const fileData = {
+      userId,
+      name,
+      type,
+      parentId,
+      isPublic,
+    };
+
+    if (type === 'folder') {
+      const newFile = await dbClient.createFile(fileData);
+      return res.status(201).json(newFile);
+    }
+
+    const folderPath = process.env.FOLDER_PATH || 'tmp/files_manager';
+    const filePath = `${folderPath}/${uuidv4()}`;
+    const fileContent = Buffer.from(data, 'base64');
+
+    try {
+      fs.writeFileSync(filePath, fileContent);
+      fileData.localPath = filePath;
+
+      const newFile = await dbClient.createFile(fileData);
+      return res.status(201).json(newFile);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+};
+
+module.exports = FilesController;
--- utils/db.js
+++ utils/db.js
@@ -42,15 +42,21 @@ class DBClient {
   }
 
   async createUser(email, password) {
-    // try {
     const db = this.client.db();
     const usersCollection = db.collection('users');
     const result = await usersCollection.insertOne({ email, password });
     return { email, _id: result.insertedId };
-    // } catch (error) {
-    //  console.error('Error creating user:', error);
-    //  throw new Error('Failed to create user');
-    // }*/
+  }
+
+  async createFile(name, type, parentId, isPublic, data) {
+    const db = this.client.db();
+    const filesCollection = db.collection('files');
+    const result = await filesCollection.insertOne({
+      name, type, parentId, isPublic, data,
+    });
+    return {
+      name, type, parentId, isPublic, _id: result.insertedId,
+    };
   }
 }
 
